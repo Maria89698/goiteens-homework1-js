2b8808f4225436c701232f1139d86ec249ca1cbb
Maria89698 <maria9parhomova@gmail.com>
2023-01-12 19:44:21 +0100

    робить дз №1

---
+++ index.js
@@ -0,0 +1,86 @@
+// 1
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+
+// 2
+const yearOfBirth = 2009;
+const name = 'Maria';
+const isModalOpen = true;
+const message = 'Hello';
+const groupName = prompt("Please enter groups name");
+
+let age = 14;
+let poits = 30;
+let mobileName = 'Apple';
+let myFriendsAge = 15;
+let value = 2;
+
+
+// 3
+const username = prompt("Ім'я користувача");
+console.log ("Ім'я користувача");
+
+
+// 4
+const myAge = 14;
+const friendsAge = 15;
+
+alert(myAge + friendsAge);
+
+// 5
+let a;
+let b;
+
+console.log(a + b);
+
+
+// 6
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+
+// 7
+const a6 = 5 % 3;
+const a7 = 3 % 5;
+const a8 = 5 + '3';
+const a9 = '5' - 3;
+const a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10); 
+
+
+// 8
+const height = 23;
+const width = 10;
+
+const s = (height * width);
+
+console.log(s);
+
+
+// 9
+const heightC = 10;
+const dC = 4;
+
+const v = (height * width);
+
+console.log(v);
